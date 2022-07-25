53d706839f6b1f0fd4b3ec6680931c4665b730b6
mspark <mspark0429@daum.net>
2022-07-25 22:29:23 +0900

    next start

---
+++ front/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'; // Next에선 없어도됨
+import AppLayout from '../components/AppLayout';
+
+const Home = () => {
+  return (
+    <AppLayout>
+      <div>Hello, Next!</div>
+    </AppLayout>
+  );
+};
+
+export default Home;
